ff62318c7b5142a1b6fb70d6ce8ddfb4cce58ee5
Saber_0324 <bryanventura0324@gmail.com>
2026-04-23 15:30:25 -0400

    added memory limit

--- cogs/code.py
+++ cogs/code.py
@@ -12,7 +12,7 @@ class Eval(commands.Cog):
         if code is None:
             await ctx.send("Correct usage: \n\`\`\`py \n<code here>\n\`\`\`")
         elif code.startswith("```py"):
-            docker_sub = subprocess.run(["docker", "run", "--rm", "python:3.12-slim", "python", "-c", f"{code[6:-4]}"], capture_output=True, text=True)
+            docker_sub = subprocess.run(["docker", "run", "--rm", "--memory='50m'", "python:3.12-slim", "python", "-c", f"{code[6:-4]}"], capture_output=True, text=True)
             await ctx.send(f"Return code: {docker_sub.returncode}. \n Output: ```{docker_sub.stdout or docker_sub.stderr}```")
         else:
             await ctx.send("Please, use the proper formatting.")
