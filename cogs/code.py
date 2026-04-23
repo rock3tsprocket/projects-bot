22c74e5a9f583da55b11a3d051c8671e53f45df8
Saber_0324 <bryanventura0324@gmail.com>
2026-04-23 15:22:30 -0400

    more minor fixing!

--- cogs/code.py
+++ cogs/code.py
@@ -12,7 +12,7 @@ class Eval(commands.Cog):
             await ctx.send("Correct usage: \n\`\`\`py \n<code here>\n\`\`\`")
         elif code.startswith("```py"):
             docker_sub = subprocess.run(["docker", "run", "--rm", "python:3.12-slim", "python", "-c", f"{code[6:-4]}"], capture_output=True, text=True)
-            await ctx.send(docker_sub.returncode)
+            await ctx.send(f"Return code: {docker_sub.returncode}. \n Output: ```{docker_sub.stdout}```")
 
 
 async def setup(bot) -> None:
