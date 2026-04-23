9564680c2cde20fdcee7ce845ba4ef2506d0a576
Saber_0324 <bryanventura0324@gmail.com>
2026-04-23 15:52:10 -0400

    added not owner check

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ async def on_command_error(ctx: commands.Context, error) -> None:
         await ctx.send("I don't have permission to do that.")
     elif isinstance(error, commands.BadArgument):
         await ctx.send("Command has a bad argument. Check all parameter are correct.")
+    elif isinstance(error, commands.NotOwner):
+        await ctx.sned("Only Saber can access this command.")
     elif isinstance(error, commands.CommandNotFound):
         pass
 
