f03996482c4ae8c4565a46a826dbfae5acbfd21e
Saber_0324 <bryanventura0324@gmail.com>
2026-04-23 15:39:17 -0400

    added not owner response

--- cogs/code.py
+++ cogs/code.py
@@ -10,29 +10,32 @@ class Eval(commands.Cog):
     @commands.command(aliases=["e"])
     @commands.is_owner()
     async def eval(self, ctx: commands.Context, *, code: str = None) -> None:
-        if code is None:
-            await ctx.send("Correct usage: \n\`\`\`py \n<code here>\n\`\`\`")
-        elif code.startswith("```py"):
-            docker_sub = subprocess.run(
-                [
-                    "docker",
-                    "run",
-                    "--rm",
-                    "--memory='50m'",
-                    "--network none",
-                    "python:3.12-slim",
-                    "python",
-                    "-c",
-                    f"{code[6:-4]}",
-                ],
-                capture_output=True,
-                text=True,
-            )
-            await ctx.send(
-                f"Return code: {docker_sub.returncode}. \n Output: ```{docker_sub.stdout or docker_sub.stderr}```"
-            )
-        else:
-            await ctx.send("Please, use the proper formatting.")
+        try:
+            if code is None:
+                await ctx.send("Correct usage: \n\`\`\`py \n<code here>\n\`\`\`")
+            elif code.startswith("```py"):
+                docker_sub = subprocess.run(
+                    [
+                        "docker",
+                        "run",
+                        "--rm",
+                        "--memory='50m'",
+                        "--network none",
+                        "python:3.12-slim",
+                        "python",
+                        "-c",
+                        f"{code[6:-4]}",
+                    ],
+                    capture_output=True,
+                    text=True,
+                )
+                await ctx.send(
+                    f"Return code: {docker_sub.returncode}. \n Output: ```{docker_sub.stdout or docker_sub.stderr}```"
+                )
+            else:
+                await ctx.send("Please, use the proper formatting.")
+        except commands.NotOwner:
+            await ctx.send(":middle_finger:")
 
 
 async def setup(bot) -> None:
