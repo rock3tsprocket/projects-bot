70344c112f42d8f028e16353314e1999c3ac840b
Saber_0324 <saber0324@proton.me>
2026-05-20 13:58:00 -0400

    Deleted views directory and moved base_view.py to templated. Added __init__.py to templates. Added command viewtest to testing.py

--- cogs/testing.py
+++ cogs/testing.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 
 from typing import TYPE_CHECKING
 
+from templates.base_view import BaseView
+
 if TYPE_CHECKING:
     from main import Hux
 
@@ -16,6 +18,16 @@ class Testing(commands.Cog):
     @app_commands.describe(text="What the bot will say is a slash command")
     async def test(self, interaction: discord.Interaction, text: str = "This"):
         await interaction.response.send_message(f"{text} is a slash command!")
+
+    @app_commands.command(name="viewtest", description="View testing.")
+    async def viewtest(self, interaction: discord.Interaction) -> None:
+        view = BaseView
+        view.add_item(
+            discord.ui.Button(label="Testing", style=discord.ButtonStyle.blurple)
+        )
+        view.message = await interaction.response.send_message(
+            "This is supposed to be a view", view=view
+        )
 
 
 async def setup(bot: Hux):
