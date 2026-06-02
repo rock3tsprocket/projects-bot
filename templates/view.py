85313c9798ec2f497e4d2b8fe37414a78d354fbe
Saber_0324 <saber0324@proton.me>
2026-06-02 11:50:47 -0400

    Added C# to langs list

--- templates/view.py
+++ templates/view.py
@@ -59,7 +59,7 @@ class BaseView(discord.ui.View):
 
 
 class CorrectUsageMenu(BaseView):
-    LANGS = ["Python", "Go", "Brainfuck", "Rust", "C", "C++"]
+    LANGS = ["Python", "Go", "Brainfuck", "Rust", "C", "C++", "C#"]
 
     @discord.ui.select(
         cls=discord.ui.Select,
