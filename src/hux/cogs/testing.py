121ab442643c2a25356a5d33f5e352898bdc4cca
Saber_0324 <saber0324@proton.me>
2026-06-03 14:51:03 -0400

    Added a message when leaving the server. Deleted unused tests. Deleted unused imports. Added logger.

--- src/hux/cogs/testing.py
+++ src/hux/cogs/testing.py
@@ -1,58 +1,25 @@
-import discord
-from discord import app_commands
 from discord.ext import commands
+import logging
 
 from typing import TYPE_CHECKING
 
-from hux.templates.view import BaseView
-
 if TYPE_CHECKING:
     from hux.main import Hux
+
+logger = logging.getLogger(__name__)
 
 
 class Testing(commands.Cog):
     def __init__(self, bot: Hux):
         self.bot = bot
 
-    @app_commands.command(name="test", description="Doing funny stuff.")
-    @app_commands.describe(text="What the bot will say is a slash command")
-    async def test(self, interaction: discord.Interaction, text: str = "This"):
-        await interaction.response.send_message(f"{text} is a slash command!")
-
-    @app_commands.command(name="viewtest", description="View testing.")
-    async def viewtest(self, interaction: discord.Interaction) -> None:
-        view = BaseView(interaction.user, timeout=10.0)
-        view.add_item(
-            discord.ui.Button(label="Testing", style=discord.ButtonStyle.blurple)
-        )
-        await interaction.response.send_message(
-            "This is supposed to be a view!", view=view
-        )
-        view.message = await interaction.original_response()
-
-    @app_commands.command(
-        name="viewerror", description="Testing error display on the BaseView"
-    )
-    async def viewerror(self, interaction: discord.Interaction) -> None:
-        view = BaseView(interaction.user, timeout=10.0)
-        button = discord.ui.Button(
-            label="Error me out!", style=discord.ButtonStyle.blurple
-        )
-
-        async def callback(interaction: discord.Interaction):
-            raise Exception(f"{interaction.user} errored me out, what a twat.")
-
-        button.callback = callback
-        view.add_item(button)
-
-        await interaction.response.send_message("Click me for an error.", view=view)
-        view.message = await interaction.original_response()
-
     @commands.command(name="leave_server")
     @commands.is_owner()
     async def leave_server(self, ctx: commands.Context):
         if ctx.guild is not None:
+            await ctx.send(f"Leaving server {ctx.guild.name}")
             await ctx.guild.leave()
+            logger.info("Left the server {ctx.guild.name} with id **{ctx.guild.id}**")
 
 
 async def setup(bot: Hux):
