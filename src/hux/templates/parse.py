87aee9dacdf84b4aa661073fcf1f9787ca1ecc09
Saber_0324 <saber0324@proton.me>
2026-06-05 22:10:25 -0400

    Trying to catch group 3 with bf stdin

--- src/hux/templates/parse.py
+++ src/hux/templates/parse.py
@@ -3,10 +3,12 @@ import re
 CODE_PATTERN = r"```+[ \t]*(.*?)[ \t]*\n(.*?)```+"
 
 
-def extract_code(pattern: str, text: str) -> tuple[str, str]:
+def extract_code(pattern: str, text: str) -> tuple[str, str] | tuple[str, str, str]:
     match = re.search(pattern, text, re.DOTALL | re.IGNORECASE)
     if match:
         if "`" in match.group(2):
             return match.group(1), "tried codeblock escaping"
+        if "bf" or "brainfuck" in match.group(1):
+            return match.group(1), match.group(2), match.group(3)
         return match.group(1), match.group(2)
     return "Language unmatched", "No code"
