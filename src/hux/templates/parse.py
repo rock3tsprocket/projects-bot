6a09c7cb67edeecb36aab5641f4dc127ddc4b7c2
Saber_0324 <saber0324@proton.me>
2026-06-05 22:58:46 -0400

    Fixed if statement

--- src/hux/templates/parse.py
+++ src/hux/templates/parse.py
@@ -9,7 +9,7 @@ def extract_code(pattern: str, text: str) -> tuple[str, str] | tuple[str, str, s
     if match:
         if "`" in match.group(2):
             return match.group(1), "tried codeblock escaping"
-        if "bf" or "brainfuck" in match.group(1):
+        if "bf" in match.group(1) or "brainfuck" in match.group(1):
             match_bf = re.search(BF_PATTERN, text, re.DOTALL | re.IGNORECASE)
             if match_bf:
                 return match_bf.group(1), match_bf.group(3), match_bf.group(3)
